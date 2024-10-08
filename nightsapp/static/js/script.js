17063004ffa89a15f015882ad37e94006f3edece
Mark Nevison <mnevison2@gmail.com>
2024-10-08 05:50:13 +0000

    added: script to display calendar and some styles

---
+++ nightsapp/static/js/script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var calendarEl = document.getElementById("calendar");
+
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: "dayGridMonth",
+    events: [
+      {
+        title: "John Doe's Holiday",
+        start: "2024-10-10",
+        end: "2024-10-15",
+        color: "green",
+      },
+      {
+        title: "Jane Smith's Holiday",
+        start: "2024-11-05",
+        end: "2024-11-10",
+        color: "green",
+      },
+    ],
+  });
+
+  calendar.render();
+});
