4432d141f90b7e7ee4e849bef0e0f659a834ae0d
Mark Nevison <mnevison2@gmail.com>
2024-10-21 05:19:33 +0000

    added: logout route and some additional authentication

--- nightsapp/auth.py
+++ nightsapp/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for, session
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
@@ -15,7 +15,7 @@ def login():
         user = User.query.filter_by(email=email).first()
         if user:
             if check_password_hash(user.password, password):
-                flash("You're now logged in!", category="success")
+                flash("Welcome back, {}!".format(user.fname), category="success")
                 login_user(user, remember=True)
                 return redirect(url_for("views.dashboard"))
             else:
@@ -28,44 +28,44 @@ def login():
 
 @auth.route("/register", methods=["GET", "POST"])
 def register():
-    print("Reached the register route") # debugging
     if request.method == "POST":
-        print("Form submitted") # debugging
         email = request.form.get("email")
         fname = request.form.get("fname")
         lname = request.form.get("lname")
         password1 = request.form.get("password1")
         password2 = request.form.get("password2")
 
-        print("Email:", email) # debugging
-        print("First Name:", fname) # debugging
-        print("Last Name:", lname) # debugging
-        print("Password1:", password1) # debugging
-        print("Password2:", password2) # debugging
-
+        # Perform validation checks
         user = User.query.filter_by(email=email).first()
         if user:
             flash("Email already registered.", category="danger")
-        elif len(email) <11:
-            flash("Minimum 10 characters required.", category="danger")
-        elif len(fname) <2:
-            flash("Name too short.", category="danger")
-        elif len(lname) <2:
-            flash("Name too short.", category="danger")
+        elif len(email) < 11:
+            flash("Email must be at least 11 characters long.", category="danger")
+        elif len(fname) < 2:
+            flash("First name is too short.", category="danger")
+        elif len(lname) < 2:
+            flash("Last name is too short.", category="danger")
         elif password1 != password2:
             flash("Passwords don't match.", category="danger")
-        elif len(password1) <7:
-            flash("Password too short, must be at least 7 characters. ", category="danger")
+        elif len(password1) < 7:
+            flash("Password too short, must be at least 7 characters.", category="danger")
         else:
+            # Create and add the new user to the database
             new_user = User(email=email, fname=fname, lname=lname, password=generate_password_hash(password1, method="pbkdf2:sha256"))
             db.session.add(new_user)
-            print("User added to session, now committing...")
             db.session.commit()
-            print("User committed to database")
             login_user(new_user, remember=True)
             flash("New Account Created!", category="success")
             return redirect(url_for("views.dashboard"))
 
+        # If validation fails, pass the form data back to the template
+        return render_template("register.html", email=email, fname=fname, lname=lname)
 
+    # Render the registration page for a GET request
+    return render_template("register.html")
 
-    return render_template ("register.html")
+@auth.route("/logout")
+def logout():
+    logout_user()
+    flash("You've been successfully logged out", category="success")
+    return redirect(url_for("auth.login"))
