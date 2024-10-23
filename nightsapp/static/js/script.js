6c9d81d7bcad9210840f9ad869f7ca60aba9c72f
Mark Nevison <mnevison2@gmail.com>
2024-10-23 03:33:11 +0000

    added functionality for tasks to display on calendar

--- nightsapp/static/js/script.js
+++ nightsapp/static/js/script.js
@@ -1,33 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var calendarEl = document.getElementById("calendar");
-
-  var calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: "dayGridMonth",
-    events: [
-      {
-        title: "John Doe's Holiday",
-        start: "2024-10-10",
-        end: "2024-10-15",
-        color: "green",
-      },
-      {
-        title: "Jane Smith's Holiday",
-        start: "2024-11-05",
-        end: "2024-11-10",
-        color: "green",
-      },
-      {
-        title: "Peter Parker's Holiday",
-        start: "2024-10-10",
-        end: "2024-10-15",
-        color: "green",
-      },
-    ],
-  });
-
-  calendar.render();
-});
-
 const deleteButtons = document.querySelectorAll('[data-bs-toggle="modal"]');
 const deleteForm = document.getElementById("deleteForm");
 const taskIdInput = document.getElementById("task_id");
