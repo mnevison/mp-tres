39cb5ad92100998501bfd2a3c9de2e1d526186a6
Mark Nevison <mnevison2@gmail.com>
2024-10-27 03:54:54 +0000

    added schema for user holidays to models.py

--- nightsapp/models.py
+++ nightsapp/models.py
@@ -11,6 +11,7 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(300), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
     is_active = db.Column(db.Boolean, default= True)
+    holidays = db.relationship('Holiday', backref='user', lazy=True)
 
     @property
     def is_authenticated(self):
@@ -42,3 +43,14 @@ class Task(db.Model):
     
     def __repr__(self):
         return f'<Task {self.title} by User {self.user_id}>'
+
+class Holiday(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    start_date = db.Column(db.DateTime, nullable=False)
+    end_date = db.Column(db.DateTime, nullable=False)
+    is_approved = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', nullable=False))
+
+    def __repr__(self):
+        return f"<UserHoliday {self.user_id} from {self.start_date} to {self.end_date}>"
