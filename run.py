a59af784415f1db85bff6113b1fc2a2e3ab152ce
Mark Nevison <mnevison2@gmail.com>
2024-11-07 01:10:30 +0000

    updated code within run.py

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from nightsapp import app
 
 if __name__ == "__main__":
     app.run(
-        host=os.getenv("IP", "0.0.0.0"),  
-        port=int(os.getenv("PORT", 5000)),
-        debug=os.getenv("DEBUG", "False").lower() == "true"
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
+        debug=os.environ.get("DEBUG")
     )
